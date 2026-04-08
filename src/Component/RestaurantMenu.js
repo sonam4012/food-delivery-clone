d81963ab727761132d3203afb4a96be223e5a372
Sonam Kumari <sonammishra1204@gmail.com>
2026-04-08 08:43:43 +0530

    custome hooks

--- src/Component/RestaurantMenu.js
+++ src/Component/RestaurantMenu.js
@@ -1,24 +1,54 @@
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { MENU_API } from "../utils/contants";
+
+// const RestaurantMenu = () =>{
+//     const [resInfo, setResInfo] = useState(null);
+
+//     const {resId} = useParams();
+
+//     useEffect(() => {
+//         fetchMenu();
+//     }, []);
+
+//     const fetchMenu = async () => {
+//         const data = await fetch(MENU_API + resId );
+//         const json = await data.json();
+//         console.log(json)
+//         setResInfo(json.data)
+//     };
+
+ 
+//     return (
+//         <div className="menu">
+//             <h1>{resInfo}</h1>
+//             <h2>Menu</h2>
+//             <ul>
+//                 <li>Biryani</li>
+//                 <li>Burgers</li>
+//                 <li>Diet Coke</li>
+//             </ul>
+
+//         </div>
+
+//     )
+// }
+
+// export default RestaurantMenu;
+
+
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/contants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () =>{
-    const [resInfo, setResInfo] = useState(null);
 
     const {resId} = useParams();
 
-    useEffect(() => {
-        fetchMenu();
-    }, []);
+    const resInfo = useRestaurantMenu(resId);
 
-    const fetchMenu = async () => {
-        const data = await fetch(MENU_API + resId );
-        const json = await data.json();
-        console.log(json)
-        setResInfo(json.data)
-    };
 
- 
     return (
         <div className="menu">
             <h1>{resInfo}</h1>
